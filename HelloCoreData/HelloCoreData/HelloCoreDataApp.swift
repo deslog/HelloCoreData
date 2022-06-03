fbe7ff17c4a45479d2b5db4d981ca944bb7073fd
JI SOO <dlwltn815@gmail.com>
2022-06-03 11:25:50 +0900

    Chore: create project

---
+++ HelloCoreData/HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by LeeJiSoo on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
